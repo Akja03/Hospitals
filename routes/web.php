2badc5c70a2b360423760e7b92dd4710f5eb1041
Akja03 <akjakakalyyewa74@gmail.com>
2024-04-29 15:25:05 -0700

    1.4.0

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HospitalController;
+use App\Http\Controllers\LocationController;
 use App\Http\Controllers\AppointmenController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
