7494c9734fd84b69f805417b5a497707da85794f
Akja03 <akjakakalyyewa74@gmail.com>
2024-04-29 15:06:04 -0700

    1.4.0

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('', [HomeController::class, 'index'])->name('home');
 
 Route::resource('/hospital', Hospitalcontroller::class);
+Route::resource('/location', Locationcontroller::class);
 Route::resource('/appointmen', Appointmencontroller::class);
 Route::resource('/category', Categorycontroller::class);
 Route::resource('/patient', Patientcontroller::class);
